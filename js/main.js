7ad4f4bb28c6823e6f4afe97933f4f79d272cf1f
Pavel-Pro <proskpavel@gmail.com>
2021-12-25 20:17:15 +0200

    экран тестимониалс

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@
 		$('.burger__item:nth-child(2)').toggleClass('burger__item-middle')
 		$('.burger__item:last-child').toggleClass('burger__item-bottom');
 	});
-	$('.factory__slider').slick({
-		infinite: true,
+	$('.testimonials__slider').slick({
+		// infinite: true,
      	dots: true,
-      	slidesToShow: 1,
-      	slidesToScroll: 1,
+      	slidesToShow: 2,
+      	slidesToScroll: 2,
       	responsive: [{
       		breakpoint: 750,
 		    	settings: {
@@ -17,24 +17,24 @@
 		    }
 	    ]
 	});
-	$(window).on('scroll', function(){
-		var menuShow = $('.veneered').offset().top;
-		var currentHeight = $(document).scrollTop();
-		if (currentHeight >= menuShow) {
-			$('.main').addClass('fixed');
-			$('.header').addClass('fixed');
-		} else {
-			$('.main').removeClass('fixed');
-			$('.header').removeClass('fixed');
-		}
-	});
-	$('.header-menu__item.dropdown').hover(function(){
-		if($(this).children('.sub-menu__inner').is(':visible')){
-			$(this).children('.sub-menu__inner').addClass('sub-menu__inner_opacity');
-		}
-		$(this).children('.sub-menu__inner').fadeIn();
-	},
-	function(){
-		$(this).children('.sub-menu__inner').delay(333).removeClass('sub-menu__inner_opacity').fadeOut();
-	}
-);
+	// $(window).on('scroll', function(){
+	// 	var menuShow = $('.veneered').offset().top;
+	// 	var currentHeight = $(document).scrollTop();
+	// 	if (currentHeight >= menuShow) {
+	// 		$('.main').addClass('fixed');
+	// 		$('.header').addClass('fixed');
+	// 	} else {
+	// 		$('.main').removeClass('fixed');
+	// 		$('.header').removeClass('fixed');
+	// 	}
+	// });
+	// $('.header-menu__item.dropdown').hover(function(){
+	// 	if($(this).children('.sub-menu__inner').is(':visible')){
+	// 		$(this).children('.sub-menu__inner').addClass('sub-menu__inner_opacity');
+	// 	}
+	// 	$(this).children('.sub-menu__inner').fadeIn();
+	// },
+	// function(){
+	// 	$(this).children('.sub-menu__inner').delay(333).removeClass('sub-menu__inner_opacity').fadeOut();
+	// }
+// );
